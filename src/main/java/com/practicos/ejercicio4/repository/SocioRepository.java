02fa5800d1d22a124b6dead4af34c246e5fead6a
TomiGrasso <tomiigrasso@gmail.com>
2026-04-15 20:31:53 -0300

    Avance de método de validación progamática

---
+++ src/main/java/com/practicos/ejercicio4/repository/SocioRepository.java
@@ -0,0 +1,25 @@
+package com.practicos.ejercicio4.repository;
+
+import com.practicos.ejercicio4.models.Socio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SocioRepository {
+
+    List<Socio> socios = new ArrayList<>();
+
+    public boolean existsByDni(String dni){
+        if (dni == null){
+            return false;
+        }
+
+        for (Socio socio : socios){
+            if (dni.equals(socio.getDni())){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
